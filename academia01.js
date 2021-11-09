935ad5de4e6b91d3eea3417c31c010d77085898c
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-09 15:34:22 -0300

    add pasta primeiro exercicio

---
+++ academia01.js
@@ -0,0 +1,43 @@
+// 01 - Mensagem codificada 
+// Elabore uma função que ao receber uma string como parâmetro retorne outra string com a posição de cada caractere
+// multiplicado por 3 no lugar do caractere. Exemplo: decodeString("Trybe") retorna "036912"
+let string = "Trybe"
+let newString = x;
+for (let i = 0) {
+newString =3*i,
+newString = newString.concat(x),
+newString += x,
+newString
+}
+// Desenvolva seu código aqui
+
+// 02 - Calcular a soma de 1 até 1000
+// Faça um algoritmo que calcule a soma de 1 a 1000 usando a estrutura "for" e retorne no formato:
+// A soma de 1 a 1000 é: X.
+
+// Desenvolva seu código aqui
+
+// 03 - Escreva seu nome N vezes
+// Faça um algoritmo usando a estrutura "FOR" que escreva 20 vezes seu nome no terminal.
+
+
+// Desenvolva seu código aqui
+
+// 04 - Números divisíveis por 3
+// Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
+
+// Desenvolva seu código aqui
+
+// 05 - Imprima os números ímpares
+//A Dora aventureira gostaria de ter uma lista com todos o números ímpares entre 1 e 50.
+//Crie um algoritmo que exibe todos os números inteiros ímpares desse intervalo.
+
+
+// Desenvolva seu código aqui
+
+// 06 - Ordem decrescente
+// Escreva um algoritmo que imprima os números inteiros do intervalo de 100 a 200 em ordem decrescente.
+
+
+// Desenvolva seu código aqui
